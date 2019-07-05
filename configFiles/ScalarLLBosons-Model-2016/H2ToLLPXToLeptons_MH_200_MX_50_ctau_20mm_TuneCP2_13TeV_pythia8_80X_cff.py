72c025ed571644886d3bf1477618aa208bfbe8b4
Celia Fernandez Madrazo <celiaafernandez95@gmail.com>
2019-07-05 11:41:37 +0200

    Add Scalar bosons model config files

---
+++ configFiles/ScalarLLBosons-Model-2016/H2ToLLPXToLeptons_MH_200_MX_50_ctau_20mm_TuneCP2_13TeV_pythia8_80X_cff.py
@@ -0,0 +1,157 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/HIG-RunIISummer15GS-01425-fragment.py --fileout file:HIG-RunIISummer15GS-01425.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions MCRUN2_71_V1::All --beamspot Realistic50ns13TeVCollision --step GEN,SIM --magField 38T_PostLS1 --python_filename HIG-RunIISummer15GS-01425_1_cfg.py --no_exec -n 151
+import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('SIM')
+
+# import of standard configurations
+#process = cms.Process('HLT',eras.Run2_25ns)
+
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic50ns13TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+# WARNING: this module is deprecated.                             # 
+ # Please use CondCore.CondDB.CondDB_cfi.py        
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(200000)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.19 $'),
+    annotation = cms.untracked.string('Configuration/GenProduction/python/HIG-RunIISummer15GS-01425-fragment.py nevts:151'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('file:EXO-H2ToLLPXToLeptons_MH_200_MX_50_ctau_20mm_TuneCP2_13TeV_pythia8_80X.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '80X_mcRun2_asymptotic_2016_TrancheIV_v8', '')
+
+
+process.generator = cms.EDFilter("Pythia8GeneratorFilter",
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring('pythia8CommonSettings',
+            'pythia8CP2Settings',
+            'processParameters'),
+        processParameters = cms.vstring('SUSY:all = off',
+            'Higgs:useBSM = on',
+            'HiggsBSM:all = off', 
+            'HiggsBSM:gg2H2 = on', 
+            '35:m0 = 200.0', 
+            '35:addChannel 1 1 100 54 54', 
+            '35:onMode = off', 
+            '35:onIfMatch = 54 54', 
+            '54:isResonance = true', 
+            '54:mWidth = 9.873179436e-15', 
+            '54:m0 = 50.0', 
+            '54:tau0 = 20.0', 
+            '54:oneChannel 1 0.5 100 11 -11', 
+            '54:addChannel 1 0.5 100 13 -13', 
+            '54:onMode = off', 
+            '54:onIfMatch = 11 -11', 
+            '54:onIfMatch = 13 -13', 
+            'Init:showProcesses = on', 
+            'Init:showChangedSettings = on',
+            'PDF:pSet = LHAPDF6:NNPDF30_lo_as_0130_nf_4'),
+        pythia8CP2Settings = cms.vstring('Tune:pp 14',
+            'Tune:ee 7',
+            'MultipartonInteractions:ecmPow=0.1391',
+            'PDF:pSet=17',
+            'MultipartonInteractions:bProfile=2',
+            'MultipartonInteractions:pT0Ref=2.306',
+            'MultipartonInteractions:coreRadius=0.3755',
+            'MultipartonInteractions:coreFraction=0.3269',
+            'ColourReconnection:range=2.323',
+            'SigmaTotal:zeroAXB=off',
+            'SpaceShower:rapidityOrder=off',
+            'SpaceShower:alphaSvalue=0.13',
+            'TimeShower:alphaSvalue=0.13'),
+        pythia8CommonSettings = cms.vstring('Tune:preferLHAPDF = 2',
+            'Main:timesAllowErrors = 10000',
+            'Check:epTolErr = 0.01',
+            'Beams:setProductionScalesFromLHEF = off',
+            'SLHA:keepSM = on',
+            'SLHA:minMassSM = 1000.',
+            'ParticleDecays:limitTau0 = on',
+            'ParticleDecays:tau0Max = 10',
+            'ParticleDecays:allowPhotonRadiation = on')
+    ),
+    comEnergy = cms.double(13000),
+    crossSection = cms.untracked.double(1),
+    filterEfficiency = cms.untracked.double(-1),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    pythiaPylistVerbosity = cms.untracked.int32(0)
+)
+
+
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.DataProcessing.Utils
+from Configuration.DataProcessing.Utils import addMonitoring 
+
+#call to customisation function addMonitoring imported from Configuration.DataProcessing.Utils
+process = addMonitoring(process)
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
+from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
+
+
